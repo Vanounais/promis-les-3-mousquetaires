531236332076f944a881a6ad6bc2d1a55de109bf
JayIsCooler <93718386+JayIsCooler@users.noreply.github.com>
2023-10-26 09:29:16 -0400

    Bannière fonctionnel
    La bannière se stock dans le localStorage

--- script.js
+++ script.js
@@ -21,3 +21,16 @@ var swiper = new Swiper(".mySwiper", {
     }
   });
   
+  let xButton = document.querySelector(".bi-x-lg")
+  let none = document.querySelector('.banner');
+
+  xButton.addEventListener("click", function() {
+    none.classList.toggle('none');
+    localStorage.setItem("etatBanniere","true");
+  });
+
+  if(localStorage.getItem("etatBanniere") != null){
+    console.log("AAAAAAAAAAAARGH");
+    none.classList.add("none");
+  }
+
