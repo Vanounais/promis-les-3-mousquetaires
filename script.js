0bce06328c3799459a635a5a6a3af59042ca2fc0
JayIsCooler <93718386+JayIsCooler@users.noreply.github.com>
2023-10-26 14:42:22 -0400

    modif
    retrait du swiper

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 60,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true
-    }, 
-     navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-    breakpoints: {
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 10
-      },
-      1024: {
-        slidesPerView: 3,
-        spaceBetween: 20
-      }
-    }
-  });
-  
   let xButton = document.querySelector(".bi-x-lg")
   let none = document.querySelector('.banner');
 
