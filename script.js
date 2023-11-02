d917a043cc65494e03b73fd1e7a74118b46fedad
v1t1o <89608462+V1ti0@users.noreply.github.com>
2023-11-02 10:15:45 -0400

    GSAP HAMBUERGER

--- script.js
+++ script.js
@@ -29,3 +29,45 @@
         disableOnInteraction: false,
     },
 });
+
+/*----------------------------------------------------- Navbar Ham ------------------------------------------------------*/
+
+
+let hambutton = document.querySelector(".navbar-toggler");
+
+let bar1 = document.querySelector(".bar1");
+
+let bar2 = document.querySelector(".bar2");
+
+let bar3 = document.querySelector(".bar3");
+
+
+let bar1anim = gsap.to(".bar1", {
+  duration: 0.2,
+  paused: true,
+  y: -1,
+});
+
+let bar3anim = gsap.to(".bar3", {
+  duration: 0.2,
+  paused: true,
+  y: 27,
+});
+
+
+/*
+let webburger = document.querySelector(".hamburger-menu");
+
+let webburgeranim = gsap.to(".hamburger-menu", {
+  duration: 0.2,
+  paused: true,
+  opacity: 0
+});
+
+*/
+
+hambutton.addEventListener("mouseenter", () => bar1anim.play());
+hambutton.addEventListener("mouseleave", () => bar1anim.reverse()); 
+
+hambutton.addEventListener("mouseenter", () => bar3anim.play());
+hambutton.addEventListener("mouseleave", () => bar3anim.reverse()); 
